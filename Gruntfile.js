b634e67524e3019d5781aab79eb9dbb8d654ce81
Selvaraj U <selvarajulaganatha@users.noreply.github.com>
2017-04-07 11:48:27 +0530

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['js/lib/jquery.min.js', 'js/main.js'],
+        dest: 'js/build/global.min.js'
+      }
+    },
+    watch: {
+      files:['js/*.js'],
+      tasks:['uglify']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'watch']);
+
+};
